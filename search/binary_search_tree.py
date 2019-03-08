e294d5cc69cd87025a3362dd116bb2a61b5c4ecc
Ta1grr <taile602@gmail.com>
2019-03-08 13:26:12 -0600

    I got most of the logic done, just need to stop the recursion properly

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -24,9 +24,54 @@ class BinarySearchTree:
     """
     # To keep track of numbers
     Queue = []
+    # Assigning the value to another variable
+    current = self.value
 
-    # If I input [3, 8, 4, 9, ]
+    # If I input [5, 3, 10, 4, 9, 11] with 5 to be the starting value
+    #
+    #            5[0]
+    #      3[1]       10[2]
+    #   4[3]       9[4]   11[5]
+    #                         
+    #
+    # Step 1: [5] , []
+    # Step 2: [3, 10] , [5]
+    # Step 3: [10, 4], [5, 3]
+    # Step 4: [4, 9, 11], [5, 3, 10]
+    # Step 5: [9, 11], [5, 3, 10, 4]
+    # Step 6: [11], [5, 3, 10, 4, 9]
+    # Step 7: [], [5, 3, 10, 4, 9, 11]
 
+    # Compare the parent node with the number we're searching for
+    # Compare parent then add in child, starting from left to right, gotta check to make sure child is there
+    # Optional I can use a while loop and just iterate until value equal cb
+
+    # Step 1: append the first value into the queue [5], []
+    if self.value is not None:
+        Queue.append(current)
+    # Step 2: Compare the first index to what we're searching for, it it is true, return the value, if it is
+    #         false then we'll add in both child nodes. i + 1 left, i + 2 right for arrays, but we can just
+    #         check self.left and self.right inside classes
+        if current == cb:
+            return Queue[0]
+        else:
+    # Step 3: If it doesn't match, remove the current node from Queue [], [5]            
+            Queue.remove(current)
+    # Step 4: append the left child [3], [5]
+            Queue.append(self.left)
+    # Step 5: append the right child [3, 10], [5]
+            Queue.append(self.right)
+    # Step 6: Check left side first and repeat from step 2 down to 5 until we find the value or return None
+            if self.left == Queue[0]:
+                self.current = self.left
+                # Remove duplicates from Queue
+                Queue.remove(self.left)
+                return self.breadth_first_for_each(cb)
+                
+            elif self.right == Queue[0]:
+                self.current = self.right
+                Queue.remove(self.right)
+                return self.breadth_first_for_each(cb)
 
 
   def insert(self, value):
